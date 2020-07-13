8b3a3d84c52f6d15122b987184b730e4316f4972
Kyle Hudson <36278207+concept-theory@users.noreply.github.com>
2020-07-12 23:40:33 -0400

    Build yarn.lock and simple update to Index page

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+       This is my first Vue project, using Nuxt.js
       </h2>
       <div class="links">
         <a
