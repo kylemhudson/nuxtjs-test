18e788d366ce694bbd30998db459f9bacce7332f
Kyle Hudson <36278207+concept-theory@users.noreply.github.com>
2020-07-25 22:47:28 -0400

    Install Netlify CMS

--- pages/index.vue
+++ pages/index.vue
@@ -367,11 +367,11 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-
 export default {
-  components: {
-    Logo,
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
   },
 };
 </script>
